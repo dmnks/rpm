eed56ceb6ba0aff22b6257d67d50c032ad59ea46
Michal Domonkos <mdomonko@redhat.com>
2023-10-23 21:56:28 +0200

    Add --shell command to rpmtests wrapper
    This invokes an interactive shell in a dummy test where all the usual
    test commands work.  This is going to be used in the next commits, no
    immediate effect here.
    
    Note that we currently do something similar in mktree.fedora for
    shell/atshell but this logic really belongs to the rpmtests wrapper
    since it basically emulates a single test.

--- tests/rpmtests.sh
+++ tests/rpmtests.sh
@@ -5,11 +5,24 @@
 #       like -C but loads atlocal from the script's directory if missing
 #   -L, --log
 #       print the test log when done
+#   -S, --shell [COMMAND]
+#       create an empty test and run a shell (or COMMAND) in it
+#   -R, --reset
+#       delete the test created with --shell
 
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 SCRIPT_FILES="atlocal mktree.common"
 
 PRINT_LOG=0
+RC=0
+
+SHELL_DIR=$PWD/rpmtests.dir/shell
+RUN_SHELL=0
+
+fixperms()
+{
+    chmod -Rf u+rwX "$@"
+}
 
 while [ $# != 0 ]; do
     case $1 in
@@ -19,6 +32,13 @@ while [ $# != 0 ]; do
         ;;
         -L | --log )
             PRINT_LOG=1
+        ;;
+        -S | --shell )
+            RUN_SHELL=1
+        ;;
+        -R | --reset )
+            rm -rf "$SHELL_DIR"
+            exit
         ;;
         *)
             break
@@ -32,9 +52,26 @@ for file in $SCRIPT_FILES; do
     [ -f "$file" ] || ln -s $SCRIPT_DIR/$file .
 done
 
-# Run the test suite
-$SCRIPT_DIR/rpmtests "$@"; RC=$?
-[ $PRINT_LOG == 1 ] && cat rpmtests.log
+# Run the test suite (or a shell)
+if [ $RUN_SHELL == 0 ]; then
+    $SCRIPT_DIR/rpmtests "$@"; RC=$?
+    [ $PRINT_LOG == 1 ] && cat rpmtests.log
+    fixperms rpmtests.dir/*/{diff,work}
+else
+    # Emulate a single, writable test (RPMTEST_SETUP)
+    set -a
+    source ./atlocal
+    trap : INT
+    RPMTEST=$SHELL_DIR/tree
+    snapshot mount $SHELL_DIR
+    if [ $# == 0 ]; then
+        $SHELL
+    else
+        "$@"
+    fi
+    snapshot umount
+    fixperms $SHELL_DIR
+fi
 
 # Clean up the symlinks
 for file in $SCRIPT_FILES; do
