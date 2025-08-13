69e0b17c4259f473a00e6279a580bc1c66b205b5
Michal Domonkos <mdomonko@redhat.com>
2025-08-13 14:13:12 +0200

    Fix addition of legacy signatures
    Commit 5630cf4bcbaae85d1b53e7a57ba6c7ed86b62e7d fixed the semantics of
    --addsign to never delete signatures, just add. This worked implicitly
    for v6 signatures but was only partially handled for legacy (v4 or v3)
    signatures.
    
    Legacy signatures can be added in the following ways:
    
    1. Signing a v4 package natively (no --rpmv6)
    2. Signing a v4 package with --rpmv6
    3. Signing a v6 package with --rpmv4
    
    Currently, we prevent the addition of multiple legacy signatures in the
    second case only, and happily allow any number of them in the other two
    cases. Since the v4 tags aren't arrays, we end up just duplicating them.
    
    Fix this by moving the haveLegacySig() check to putSignature() so that
    we catch all legacy signature additions. Make sure to keep the identical
    signature warning, though, to preserve the historical behavior and make
    it easier to backport this patch to the RPM 4.x series.
    
    Note that we have to check for the correct tags in haveLegacySig() now
    since v3 signatures are only added after the v4 ones in addSignature()
    and we'd otherwise always skip them.
    
    See the updated tests for the use cases changed or fixed.
    
    Fixes: 3878

--- sign/rpmgensig.cc
+++ sign/rpmgensig.cc
@@ -397,6 +397,12 @@ static int haveSignature(rpmtd sigtd, Header sigh)
     return rc;
 }
 
+static int haveLegacySig(Header sigh, int ishdr)
+{
+    return headerIsEntry(sigh, ishdr ? RPMSIGTAG_RSA : RPMSIGTAG_PGP) ||
+	   headerIsEntry(sigh, ishdr ? RPMSIGTAG_DSA : RPMSIGTAG_GPG);
+}
+
 static int putSignature(Header sigh, rpmtd sigtd, int multisig, int ishdr,
 			rpmSignFlags flags)
 {
@@ -441,7 +447,9 @@ static int putSignature(Header sigh, rpmtd sigtd, int multisig, int ishdr,
 	if (sigtag) {
 	    sigtd->tag = sigtag;
 	    if (haveSignature(sigtd, sigh)) {
-		rc = 1;
+		rc = (flags & RPMSIGN_FLAG_RPMV6) ? 0 : 1;
+	    } else if (haveLegacySig(sigh, ishdr)) {
+		rc = 0;
 	    } else {
 		rc = (headerPut(sigh, sigtd, HEADERPUT_DEFAULT) == 0) ? -1 : 0;
 	    }
@@ -459,12 +467,6 @@ static int putSignature(Header sigh, rpmtd sigtd, int multisig, int ishdr,
     }
 
     return rc;
-}
-
-static int haveLegacySig(Header sigh)
-{
-    return headerIsEntry(sigh, RPMSIGTAG_RSA) ||
-	   headerIsEntry(sigh, RPMSIGTAG_DSA);
 }
 
 static int addSignature(Header sigh, rpmSignFlags flags,
@@ -706,9 +708,6 @@ static int rpmSign(const char *rpm, int deleting, int flags)
     } else {
 	flags |= RPMSIGN_FLAG_RPMV4;
 	reserveTag = RPMSIGTAG_RESERVEDSPACE;
-	/* Ensure only one legacy signature is added if adding v6 signatures */
-	if ((flags & RPMSIGN_FLAG_RPMV6) && haveLegacySig(sigh))
-	    flags &= ~(RPMSIGN_FLAG_RPMV4|RPMSIGN_FLAG_RPMV3);
     }
 
     if (headerIsSource(h)) {
